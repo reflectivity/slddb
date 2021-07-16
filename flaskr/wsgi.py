e7658d50fbc0478971fd39d5d664341e7c9e9c3c
Artur Glavic <artur.glavic@psi.ch>
2021-07-16 19:24:15 +0200

    Remove unnecessary sys.path inserts that might lead to undesired side-effects (import of sub-package as module)

--- flaskr/wsgi.py
+++ flaskr/wsgi.py
@@ -1,5 +1,3 @@
 import sys
 sys.path.insert(0, "/var/www/html/slddb")
-sys.path.insert(0, "/var/www/html/slddb/flaskr")
-sys.path.insert(0, "/var/www/html/slddb/flaskr/templates")
 from flaskr import app as application
