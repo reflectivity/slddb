d49f0c05e77082b571bbee9fed1fc43a2d6e54aa
Artur Glavic <artur.glavic@psi.ch>
2023-10-03 18:22:00 +0200

    minor test

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ from flaskr import app
 import os
 
 def application(env, start_response):
-    print(os.environ)
-    print(env)
-    return app(env, start_response)
+    output=app(env, start_response)
+    print(app.config)
+    return output
