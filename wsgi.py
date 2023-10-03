6421acd09d96f458a2383499ad0f8dc04639bc43
Artur Glavic <artur.glavic@psi.ch>
2023-10-03 18:16:03 +0200

    Test updating wsgi script to show variables.

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,4 @@ from flaskr import app
 import os
 
 def appplication(env, start_response):
-    print(os.environ)
-    print(env)
-    return app(env, start_response)
+    return app(env, start_response)+[repr(os.environ), repr(env)]
